5f812bae20e8e3e8a6e0d344f830656e78d06437
Kambm <masonify@gmail.com>
2017-01-22 11:21:19 -0800

    added stuff

--- src/main/SimulatorMain.java
+++ src/main/SimulatorMain.java
@@ -3,6 +3,7 @@ package main;
 import java.util.*;
 
 public class SimulatorMain {
+	public static final double dt = 0.01;
 	ArrayList<LaneManager> lanes;
 	
 	public LaneManager getLane(int laneRank) {
