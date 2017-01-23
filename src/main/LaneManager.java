3294719ae1c1d8284f12741ddead4cf7ebfa1465
George_Stepaniants <George_Stepaniants@d-69-91-134-36.dhcp4.washington.edu>
2017-01-22 16:52:07 -0800

    Working on LaneManager

--- src/main/LaneManager.java
+++ src/main/LaneManager.java
@@ -33,7 +33,7 @@ public class LaneManager {
     	}
     	
     	if (simulator.time % this.processingRate == 0) {
-			if (!queue.isEmpty())
+			if (!queue.isEmpty()) {
 	    		Vehicle car = queue.remove();
 	    		if (!vehicles.isEmpty()) {
 	    			car.velocity = vehicles.get(1).velocity;
@@ -44,6 +44,7 @@ public class LaneManager {
 	    		vehicles.add(car);
     		}
     	}
+    	
     	
     	
         /*for (int i = vehicles.size() - 1; i >= 0; i--) {
@@ -76,30 +77,30 @@ public class LaneManager {
     
     public void edgeUpdate(int i) { // updates vehicle i in an edge lane
         Vehicle v = vehicles.get(i);
-        if (v.shouldMergeEdge(manager.getLane(neighor))) {
-            merge(i, v, manager.getLane(neighor));
+        if (v.shouldMergeEdge(simulator.getLane(neighor))) {
+            merge(i, v, simulator.getLane(neighor));
         }
     }
     
     public void centerUpdate(int i) { // updates vehicle i in a center lane
         Vehicle v = vehicles.get(i);
-        if (v.shouldMergeCenter(manager.getLane(laneRank - 1)) && v.shouldMergeCenter(manager.getLane(laneRank + 1))) {
+        if (v.shouldMergeCenter(simulator.getLane(laneRank - 1)) && v.shouldMergeCenter(simulator.getLane(laneRank + 1))) {
             if ((new Random()).nextBoolean()) {
-                merge(i, v, manager.getLane(laneRank - 1));
+                merge(i, v, simulator.getLane(laneRank - 1));
             } else {
-                merge(i, v, manager.getLane(laneRank + 1));
+                merge(i, v, simulator.getLane(laneRank + 1));
             }
-        } else if (v.shouldMergeCenter(manager.getLane(laneRank + 1))) {
-            merge(i, v, manager.getLane(laneRank + 1));
-        } else if (v.shouldMergeCenter(manager.getLane(laneRank - 1))) {
-            merge(i, v, manager.getLane(laneRank - 1));
+        } else if (v.shouldMergeCenter(simulator.getLane(laneRank + 1))) {
+            merge(i, v, simulator.getLane(laneRank + 1));
+        } else if (v.shouldMergeCenter(simulator.getLane(laneRank - 1))) {
+            merge(i, v, simulator.getLane(laneRank - 1));
         } else {
             v.position += v.velocity * Simulator.dt;
         }
     }
     
     public boolean centerShouldMerge(int i, int laneRank) {
-        LaneManager lane = manager.getLane(laneRank);
+        LaneManager lane = simulator.getLane(laneRank);
         Vehicle v = vehicles.get(i);
         return (!lane.edgeLane) && v.shouldMergeCenter(lane);
     }
